f2143a49736b447a7b877ff9077ada9daa91be2e
ArfOz <67826200+ArfOz@users.noreply.github.com>
2021-12-13 22:36:21 +0300

    Add files via upload

--- Questions.js
+++ Questions.js
@@ -1,10 +1,11 @@
 
 //Questions 2/a
-const num = Math.floor(Math.random() * 6) + 1;
-console.log("questions", num)
+const d = new Date();
+let time = d.getTime();
+console.log("questions", time%6 + 1)
 
 //Questions 2/b I will write explicitly. At first I start function. So I expect that result have to be between 0-6. I can write this mocha.js
 
 //Questions3 I didnt know the difference. but I searched text files are human readeble. But binary is not. 
 
-//Bonus question I think trains go to the west side are 4 times more to the trains go to the east side.
+//Bonus question I thinkto west side trains the every 00 minutes, For ex: 00, 10, 20,30 40,50 while east side trains goes every 8th minutes. Such as: 08,18,28,38,48,58
